682d5eb40d326cbb7f235d9c4f0d04b5f063cb7a
Alreim <57833694+Whatever-2@users.noreply.github.com>
2026-04-04 12:00:30 +0800

    Made Basic Main Menu && Arranged Files

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void loadScenByNumber(int sceneNumber)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneNumber);
+    }   
+
+    public  void LoadNextScene()
+    {
+        int currentSceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadPreviousScene()
+    {
+        int currentSceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
